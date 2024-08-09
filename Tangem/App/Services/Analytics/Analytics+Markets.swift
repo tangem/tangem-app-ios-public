c0588c32f67cbdfcc28a494b5586dc504b3e253a
Service Account <service@tangem.com>
2024-08-09 22:30:21 +0300

    Description removed

---
+++ Tangem/App/Services/Analytics/Analytics+Markets.swift
@@ -0,0 +1,17 @@
+//
+//  Analytics+Markets.swift
+//  Tangem
+//
+//  Created by skibinalexander on 07.08.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+extension Analytics {
+    enum MarketsIntervalTypeSourceType: String {
+        case insights = "Insights"
+        case price = "Price"
+        case chart = "Chart"
+    }
+}
