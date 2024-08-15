ccb3d3bc1adb9e935e7a2e64c09aea91bd7fb8d1
Service Account <service@tangem.com>
2024-08-15 17:40:05 +0400

    Description removed

---
+++ TangemStaking/Models/AdditionalAddresses.swift
@@ -0,0 +1,13 @@
+//
+//  AdditionalAddresses.swift
+//  TangemStaking
+//
+//  Created by Dmitry Fedorov on 14.08.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+public struct AdditionalAddresses {
+    let cosmosPubKey: String?
+}
