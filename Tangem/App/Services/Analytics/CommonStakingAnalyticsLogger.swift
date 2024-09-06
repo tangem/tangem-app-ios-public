87953647087d22ec30dbe4b0c605dbf82495e22b
Service Account <service@tangem.com>
2024-09-06 21:27:05 +0400

    Description removed

---
+++ Tangem/App/Services/Analytics/CommonStakingAnalyticsLogger.swift
@@ -0,0 +1,16 @@
+//
+//  CommonStakingAnalyticsLogger.swift
+//  Tangem
+//
+//  Created by Dmitry Fedorov on 06.09.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import TangemStaking
+
+struct CommonStakingAnalyticsLogger: StakingAnalyticsLogger {
+    func logAPIError() {
+        Analytics.log(.stakingErrors)
+    }
+}
