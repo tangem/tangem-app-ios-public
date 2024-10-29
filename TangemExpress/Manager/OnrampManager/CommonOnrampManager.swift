221637cc038e75fd9b9bf2a2c1b6f3c317f10ffb
Service Account <service@tangem.com>
2024-10-29 15:36:02 +0000

    Description removed

--- TangemExpress/Manager/OnrampManager/CommonOnrampManager.swift
+++ TangemExpress/Manager/OnrampManager/CommonOnrampManager.swift
@@ -27,10 +27,14 @@ public actor CommonOnrampManager {
 // MARK: - OnrampManager
 
 extension CommonOnrampManager: OnrampManager {
-    public func updateCountry() async throws -> OnrampCountry {
-        // Define country by ip or get from repository
-        // https://tangem.atlassian.net/browse/IOS-8267
-        throw OnrampManagerError.notImplement
+    public func getCountry() async throws -> OnrampCountry {
+        if let country = onrampRepository.savedCountry {
+            return country
+        }
+
+        let country: OnrampCountry = try await provider.onrampCountryByIP()
+        onrampRepository.updatePreference(country: country)
+        return country
     }
 
     public func updatePaymentMethod() async throws -> OnrampPaymentMethod {
